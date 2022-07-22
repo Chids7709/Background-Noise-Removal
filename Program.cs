7f8ba6026437679c57e6866b2a54b60f51af212a
Chids7709 <59773897+Chids7709@users.noreply.github.com>
2022-07-22 15:53:36 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: chidi
+ * Date: 21/07/2022
+ * Time: 21:21
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System;
+using System.Windows.Forms;
+
+namespace STM32_FFT_Spectral_Analysis
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
+
